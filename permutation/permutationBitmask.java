90613f8278a4dc8455ec7a5fdc83afa5d2ad63ff
jamie <rmk1075@gmail.com>
2020-03-10 14:36:29 +0900

    permutation

---
+++ permutation/permutationBitmask.java
@@ -0,0 +1,37 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class permutationBitmask {
+    static int N, R, totalCnt;
+    static int[] input, number;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+        R = sc.nextInt();
+        input = new int[N];
+        number = new int[R];
+
+        for (int i = 0; i < N; i++)
+            input[i] = sc.nextInt();
+
+        permutation(0, 0);
+        System.out.println("total count : " + totalCnt);
+    }
+
+    static void permutation(int count, int flag) {
+        if (count == R) {
+            totalCnt++;
+            System.out.println(Arrays.toString(number));
+            return;
+        }
+
+        for (int i = 0; i < N; i++) {
+            if ((flag & (1 << i)) != 0) continue;
+
+            number[count] = input[i];
+            flag |= (1 << i);
+            permutation(count + 1, flag);
+        }
+    }
+}
---
+++ permutation/permutationBooleanArrays.java
@@ -0,0 +1,39 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class permutationBooleanArrays {
+    static int N, R, totalCnt;
+    static int[] input, number;
+    static boolean[] isSelected;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+        R = sc.nextInt();
+        input = new int[N];
+        number = new int[R];
+        isSelected = new boolean[N];
+
+        for(int i = 0; i < N; i++) 
+            input[i] = sc.nextInt();
+        
+        permutation(0);
+        System.out.println("total count : " + totalCnt);
+    }
+
+    static void permutation(int count) {
+        if(count == R) {
+            totalCnt++;
+            System.out.println(Arrays.toString(number));           
+            return ;
+        }
+
+        for(int i = 0; i < N; i++) {
+            if(isSelected[i]) continue;
+
+            number[count] = input[i];
+            isSelected[i] = true;
+            permutation(count+1);
+            isSelected[i] = false;
+        }
+    }
+}
